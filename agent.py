81e439e286166aa6cfe2f50f010588d2a2058ca6
Tazi Mohannad <77018169+mohannadtazi@users.noreply.github.com>
2024-10-11 18:45:33 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -8,21 +8,19 @@ load_dotenv()
 # Fetch the API key from the environment variables
 os.environ['GROQ_API_KEY']= os.getenv("GROQ_API_KEY")
 api_key = os.getenv("GROQ_API_KEY")
-model='groq/llama3-70b-8192'
+global model, temperature
+model='llama3-70b-8192'
 temperature=0.0
 
-# Function to dynamically set the model and temperature
-def initialize_agent(selected_model, selected_temperature):
-    global llm, researcher, writer, designer
-    
-    llm = ChatGroq(
-        model=selected_model,
+
+llm = ChatGroq(
+        model=model,
         api_key=api_key,
         temperature=selected_temperature
     )
 
 
-    researcher= Agent(
+researcher= Agent(
 role="Senior Researcher",
 goal="""Uncover groundbreaking topics in {topic}""", 
 backstory="""Driven by curiosity, you are at the forefront of Innovation sharing knowledge and news that would change the world""",
@@ -30,14 +28,14 @@ llm=llm,
 verbose=True,memory=True,)
 
 
-    writer= Agent(
+writer= Agent(
 role="Writer",
 goal="""Narrate compelling stories following the style: {style}, and it should be {length}, addapted for {platform} platform""", 
 backstory="""Your expertise lies in breaking down complex topics into simpler and digestable knowledge, you specialize in captivativing audience attention with your writing and create engaging narratives and stories""",
 llm=llm,
 verbose=True,memory=True)
 
-    designer= Agent(
+designer= Agent(
 role="Designer",
 goal="""Craft prompts to generate visually appealing images about {topic} in the {style} style for the post for {platform} platform.""",
 backstory="""You are a prompt engineer, you specialize in creating visually appealing images that captivate the audience and engage them""",
